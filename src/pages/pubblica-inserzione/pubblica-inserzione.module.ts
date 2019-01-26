b8d2089b7d89ed1abaee34c954644482b92ad8d4
mattefic <matteo.ficorilli@libero.it>
2019-01-26 12:00:18 +0100

    PubblicaInserzionePage

---
+++ src/pages/pubblica-inserzione/pubblica-inserzione.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PubblicaInserzionePage } from './pubblica-inserzione';
+
+@NgModule({
+  declarations: [
+    PubblicaInserzionePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PubblicaInserzionePage),
+  ],
+})
+export class PubblicaInserzionePageModule {}
